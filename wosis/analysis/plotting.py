aaa92ec03a4bf32753aab4467a73183c2bf791e5
Takuya Iwanaga <takuyai@gmail.com>
2018-10-19 14:23:47 +1100

    Tidied up code and filling in missing years in data

--- wosis/analysis/plotting.py
+++ wosis/analysis/plotting.py
@@ -6,11 +6,6 @@ from matplotlib.ticker import MaxNLocator
 
 from .search import get_unique_kw_titles
 
-sns.set_style('darkgrid')
-sns.set_context('paper', font_scale=1.4)
-
-plt.tight_layout()
-
 
 def plot_saver(func):
     """Decorator to enable all plotting functions to save figures"""
@@ -22,6 +17,8 @@ def plot_saver(func):
         if save_plot_fn:
             if save_plot_fn.endswith('.png'):
                 save_plot_fn = save_plot_fn.strip('.png')
+
+            plt.tight_layout()
             plot.savefig(save_plot_fn+'.png', format='png', dpi=300)
         # End if
     # End wrapper()
@@ -31,7 +28,7 @@ def plot_saver(func):
 
 
 @plot_saver
-def plot_kw_trend(search_results):
+def plot_kw_trend(search_results, title=None):
     """Plot keyword trends across time.
 
     Parameters
@@ -46,37 +43,44 @@ def plot_kw_trend(search_results):
     ==========
     * matplotlib figure object
     """
-    trend = {}
-    num_pubs = {}
-    for rec in search_results:
-        year = rec.get('PY')  # Get publication year
-        if year not in trend:
-            trend[year] = 0
-            num_pubs[year] = 0
+    time_series = search_results.timeSeries(pandasMode=True)
 
-        num_all_kwds = len(rec.get('DE')) + len(rec.get('ID'))
-        trend[year] += num_all_kwds
-        num_pubs[year] += 1
+    num_kwds = [len(ent['DE']) + len(ent['ID']) for ent in time_series['entry']]
+    yearly = pd.DataFrame({'year': time_series['year'], 'count': num_kwds}).groupby('year')
+    num_pubs = yearly.count().sort_index()
+    kw_trend = pd.DataFrame({'year': time_series['year'], 'count': num_kwds}).groupby('year').sum().sort_index()
+    avg_kw_per_pub = (kw_trend.loc[:, 'count'] / num_pubs.loc[:, 'count']).to_frame()
 
-    avg_kw_per_pub = np.array(list(trend.values())) / np.array(list(num_pubs.values()))
-
-    df = pd.DataFrame({'Num. Keywords': avg_kw_per_pub}, index=trend.keys()).sort_index()
+    # Fill in the missing years
+    min_year, max_year = kw_trend.index.min(), kw_trend.index.max()
+    idx = pd.period_range(min_year, max_year, freq='Y')
+    avg_kw_per_pub = pd.DataFrame({'count': [avg_kw_per_pub.loc[i, 'count']
+                                             if i in avg_kw_per_pub.index else 0 for i in idx.year]}, index=idx)
+    num_pubs = pd.DataFrame({'count': [num_pubs.loc[i, 'count'] if i in num_pubs.index else 0 for i in idx.year]},
+                            index=idx)
 
     fig, (ax1, ax2) = plt.subplots(1,2)
-    rot = 45 if len(trend) < 20 else 90
 
-    df.plot(kind='bar', figsize=(18, 6), rot=rot, title="Average Number of Keywords per Publication", ax=ax1, legend=False)
+    # Rotate x-axis labels if there is enough space
+    rot = 45 if len(kw_trend) < 20 else 90
+    fig.subplots_adjust(top=0.3)  # make space for top title
+
+    pub_data = num_pubs.loc[:, 'count']
+    num_text = "Total Number of Publications: {}".format(pub_data.sum())
+    if title:
+        title = title+'\n'+num_text
+    else:
+        title = num_text
+    plt.suptitle(title, fontsize='22')
+
+    avg_kw_per_pub.plot(kind='bar', figsize=(18, 6), rot=rot, ax=ax1, legend=False)
     ax1.set_xlabel("Year")
     ax1.set_ylabel("Average Num. Keywords");
 
+    log_form = True if max(pub_data) > 100 else False  # use log scale if large values found
+
     ax2.yaxis.set_major_locator(MaxNLocator(integer=True))  # force y-axis to use integer values
-    pub_data = num_pubs.values()
-    num_pubs = pd.DataFrame({'Num. Publications': list(pub_data)}, index=num_pubs.keys()).sort_index()
-    num_pub_title = "Number of Publications\nTotal: {}".format(sum(pub_data))
-
-    log_form = True if max(pub_data) > 100 else False
-
-    num_pubs.plot(kind='bar', ax=ax2, rot=rot, logy=log_form, legend=False, title=num_pub_title)
+    num_pubs.plot(kind='bar', ax=ax2, rot=rot, logy=log_form, legend=False)
     ax2.set_xlabel("Year")
     ax2.set_ylabel("Num. Publications")
 
@@ -112,12 +116,12 @@ def plot_pub_per_kw(ind_recs, summary, corpora, kw_category, annotate=False):
     pubs_per_kw = pd.DataFrame(list(summary.items()), index=list(summary.keys()), columns=['Keyword', 'Count'])
     pubs_per_kw.sort_values(by='Count', inplace=True)
 
-    ax = pubs_per_kw.plot(kind='bar', title=ptitle, rot=45, figsize=(8,6))
+    ax = pubs_per_kw.plot(kind='bar', title=ptitle, figsize=(8,6))
 
     if annotate:
         # Annotate number above bar
         for p in ax.patches:
-            ax.annotate("{}".format(p.get_height()), (p.get_x() + 0.01, p.get_height() * 1.015))
+            ax.annotate("{}".format(p.get_height()), (p.get_x() + 0.01, p.get_height() * 1.015), fontsize=14)
 
     ax.yaxis.set_major_locator(MaxNLocator(integer=True))  # force y-axis to use integer values
 
