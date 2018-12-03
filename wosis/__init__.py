d5014430097073d05e37bb70c2fc378c97a01b87
Takuya Iwanaga <takuyai@gmail.com>
2018-12-03 19:06:45 +1100

    clean package import

--- wosis/__init__.py
+++ wosis/__init__.py
@@ -1,4 +1,4 @@
 from .query import *
 from .store import *
-
+from .convert import *
 from .analysis import *
--- wosis/analysis/__init__.py
+++ wosis/analysis/__init__.py
@@ -1,1 +1,2 @@
 from .search import *
+from .constrain import find_topics, display_topics, get_topic_by_id
